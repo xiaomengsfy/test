aaea9f03186e1c566b2748350f7fa994d6519864
xiaomengsfy <138100953+xiaomengsfy@users.noreply.github.com>
2023-07-26 02:31:15 +0800

    Update xinxiliu.js

--- xinxiliu.js
+++ xinxiliu.js
@@ -49,5 +49,5 @@ hostname = app.bilibili.com
 
 
 //video_mode=2 首页信息流全是短视频，即每个点进去都是Story模式
-var modifiedPath = $request.path.replace(/video_mode=\d/g,'video_mode=1');
+var modifiedPath = $request.path.replace(/video_mode=\d/g,'video_mode=2');
 $done({path: modifiedPath});
