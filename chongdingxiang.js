c9dc6c6021db28c3a79a9bc606e0618ce02c2fb5
xiaomengsfy <138100953+xiaomengsfy@users.noreply.github.com>
2024-10-30 20:19:02 +0800

    Update chongdingxiang.js

--- chongdingxiang.js
+++ chongdingxiang.js
@@ -1,22 +1,20 @@
 // ==UserScript==
 // @name         自用网页重定向
-// @version      0.1.1
-// @updateURL    https://raw.githubusercontent.com/xiaomengsfy/test/refs/heads/main/%E9%87%8D%E5%AE%9A%E5%90%91.js
-// @downloadURL  https://raw.githubusercontent.com/xiaomengsfy/test/refs/heads/main/%E9%87%8D%E5%AE%9A%E5%90%91.js
+// @version      0.1.2
 // @description  重定向部分网页方便日常使用
 // @match        *://*.bilibili.com/list*
-// @match        *://*.google.cn/*
-// @match        *://acg.tv/*
-// @match        *://baike.baidu.hk/*
-// @match        *://*.google.com/search?q=*
+// @match        
+// @match        
+// @match        
+// @match        *://*.google.com/search?q=mnbk*
 // @match        *://jm.tt/*
-// @match        *://zh.moegirl.org.cn/*
-// @match        *://zh.wikipedia.org/*
+// @match        
+// @match        
 // @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
     var url = window.location.href;
-    window.location.href = url.replace('jm.tt', '18comic.vip').replace(/\/list\/watchlater\?bvid=([^&]+).*/, '/video/$1');
+    window.location.href = url.replace('jm.tt', '18comic.vip').replace(/\/list\/watchlater\?bvid=([^&]+).*/, '/video/$1').replace(/.*\.google.*/search?q=mnbk.*/, 'zh.moegirl.org.cn/index.php?search=').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '').replace('', '')v;
 })();
